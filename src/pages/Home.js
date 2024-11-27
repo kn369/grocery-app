b2a0da27be1af83d85ead3a94f154b91b41e2dbd
Poojan Patel <patelpoojan308@gmail.com>
2024-11-27 07:03:14 +0530

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,50 @@
+// import React from "react";
+// import CustomNavbar from "../components/CustomNavbar";
+// import Title from "../components/Title";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+// import CardComponent from "../components/CardComponent";
+
+// const Home = () => {
+// 	const [categories, setCategories] = useState([]);
+
+// 	useEffect(() => {
+// 		fetchCategories();
+// 	}, []);
+
+// 	const fetchCategories = async () => {
+// 		try {
+// 			const response = await axios.get("http://localhost:3000/categories");
+// 			setCategories(response.data);
+// 		} catch (error) {
+// 			console.log("Error loading categories ...");
+// 		}
+// 	};
+// 	return (
+// 		<>
+// 			<CustomNavbar />
+// 			<Title />
+// 			<div>
+// 				<h1 style={{ display: "flex", justifyContent: "center", margin: "1vw" }}>
+// 					Shop by Category
+// 				</h1>
+// 				<div style={{display: "grid", gridTemplateColumns: "repeat(4, 1fr)", margin: "1vw"}} >
+// 					{categories.map((category) => {
+// 						console.log(category);
+// 						return (
+// 							<div style={{maxWidth: "40vh", padding: "0.5vh"}}>
+// 								<CardComponent {...category} />
+// 							</div>
+// 						);
+// 					})}
+// 				</div>
+// 			</div>
+// 		</>
+// 	);
+// };
+
+// export default Home;
+
 import React from "react";
 import CustomNavbar from "../components/CustomNavbar";
 import Title from "../components/Title";
@@ -14,31 +61,37 @@ const Home = () => {
 
 	const fetchCategories = async () => {
 		try {
-			const response = await axios.get("http://localhost:3000/categories");
-			setCategories(response.data);
+		const response = await axios.get("http://localhost:3000/categories");
+		setCategories(response.data);
 		} catch (error) {
-			console.log("Error loading categories ...");
+		console.log("Error loading categories ...");
 		}
 	};
+
 	return (
 		<>
-			<CustomNavbar />
-			<Title />
-			<div>
-				<h1 style={{ display: "flex", justifyContent: "center", margin: "1vw" }}>
-					Shop by Category
-				</h1>
-				<div style={{display: "grid", gridTemplateColumns: "repeat(4, 1fr)", margin: "1vw"}} >
-					{categories.map((category) => {
-						console.log(category);
-						return (
-							<div style={{maxWidth: "40vh", padding: "0.5vh"}}>
-								<CardComponent {...category} />
-							</div>
-						);
-					})}
+		<CustomNavbar />
+		<Title />
+		<div style={{ textAlign: "center", margin: "2rem 0" }}>
+			<h1>Shop by Category</h1>
+			<br />
+			<div
+			style={{
+				display: "grid",
+				gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
+				gap: "1.5rem",
+				padding: "0 1rem",
+			}}
+			>
+			{categories.map((category) => {
+				return (
+				<div key={category.id} style={{ maxWidth: "100%" }}>
+					<CardComponent {...category} />
 				</div>
+				);
+			})}
 			</div>
+		</div>
 		</>
 	);
 };
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -7,18 +7,10 @@ const Profile = () => {
 	const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
 	const [isEditing, setIsEditing] = useState(null);
 	const [editValue, setEditValue] = useState("");
-	const [newAddress, setNewAddress] = useState("");
-	const [editingAddressIndex, setEditingAddressIndex] = useState(null);
-	const [editingAddressValue, setEditingAddressValue] = useState("");
 
 	const handleEdit = (field) => {
 		setIsEditing(field);
 		setEditValue(user[field]);
-	};
-
-	const handleEditAddress = (index) => {
-		setEditingAddressIndex(index);
-		setEditingAddressValue(user.addresses[index]);
 	};
 
 	const handleSave = async (field) => {
@@ -35,65 +27,6 @@ const Profile = () => {
 		}
 	};
 
-	const handleDeleteAddress = async (index) => {
-		const updatedUser = {
-			...user,
-			addresses: user.addresses.filter((_, i) => i !== index),
-		};
-		try {
-			await axios.put(`http://localhost:3000/users/${user.id}`, updatedUser);
-			setUser(updatedUser);
-			localStorage.setItem("user", JSON.stringify(updatedUser));
-			alert("Address deleted successfully");
-		} catch (error) {
-			console.error("Error deleting address:", error);
-			alert("Error deleting address");
-		}
-	};
-
-	const handleSaveAddress = async (index) => {
-		const updatedAddresses = user.addresses.map((address, i) =>
-			i === index ? editingAddressValue : address
-		);
-		const updatedUser = { ...user, addresses: updatedAddresses };
-		try {
-			await axios.put(`http://localhost:3000/users/${user.id}`, updatedUser);
-			setUser(updatedUser);
-			localStorage.setItem("user", JSON.stringify(updatedUser));
-			setEditingAddressIndex(null);
-			alert("Address updated successfully");
-		} catch (error) {
-			console.error("Error updating address:", error);
-			alert("Error updating address");
-		}
-	};
-
-	const handleAddAddress = async (event) => {
-		event.preventDefault();
-
-		if (!newAddress) {
-			alert("Please enter an address");
-			return;
-		}
-
-		const updatedUser = {
-			...user,
-			addresses: user.addresses
-				? [...user.addresses, newAddress]
-				: [newAddress],
-		};
-		console.log(updatedUser);
-		try {
-			await axios.put(`http://localhost:3000/users/${user.id}`, updatedUser);
-			setUser(updatedUser);
-			localStorage.setItem("user", JSON.stringify(updatedUser));
-			alert("Address added successfully");
-			window.location.reload();
-		} catch (error) {
-			console.error("Error adding address:", error);
-			alert("Error adding address");
-		}
-	};
 	if (!user) {
 		return (
 			<p>
@@ -127,7 +60,7 @@ const Profile = () => {
 					width: "80%",
 				}}
 			>
-				{["name", "email", "phone"].map((field) => (
+				{["name", "email", "phone", "addresses"].map((field) => (
 					<div
 						key={field}
 						style={{
@@ -174,104 +107,20 @@ const Profile = () => {
 						)}
 					</div>
 				))}
-				<h2 style={{ color: "#555", margin: "10px 0" }}>Addresses</h2>
-				{user.addresses ? (
-					user.addresses.map((address, index) => (
-						<div
-							key={index}
-							style={{
-								margin: "10px 0",
-								color: "#555",
-								display: "flex",
-								justifyContent: "space-between",
-								alignItems: "center",
-							}}
-						>
-							<p style={{ margin: "0 10px 0 0", fontSize: "1.5rem" }}>
-								{index + 1}
-							</p>
-							<div
-								style={{
-									display: "flex",
-									alignItems: "center",
-									justifyContent: "space-between",
-									width: "100%",
-								}}
-							>
-								{editingAddressIndex === index ? (
-									<>
-										<input
-											type="text"
-											value={editingAddressValue}
-											onChange={(e) => setEditingAddressValue(e.target.value)}
-											style={{
-												border: "none",
-												width: "100%",
-                                    background: "#f0f0f0",
-                                    fontSize: "1.5rem"
-											}}
-										/>
-										<button
-											style={{ background: "0", border: "none" }}
-											onClick={() => handleSaveAddress(index)}
-										>
-											Save
-										</button>
-									</>
-								) : (
-									<>
-										<p style={{ margin: "0 10px 0 0", fontSize: "1.5rem" }}>
-											{address}
-										</p>
-										<div style={{ display: "flex", alignItems: "center" }}>
-											<button
-												style={{
-													background: "0",
-													border: "none",
-													marginRight: "0.5rem",
-												}}
-												onClick={() => handleEditAddress(index)}
-											>
-												🖊️
-											</button>
-											<Button
-												variant="danger"
-												onClick={() => handleDeleteAddress(index)}
-												style={{
-													height: "1.5rem",
-													width: "1.5rem",
-													padding: "0",
-													display: "flex",
-													alignItems: "center",
-													justifyContent: "center",
-													fontSize: "1rem",
-													lineHeight: "1rem",
-													marginRight: "0.5rem",
-												}}
-											>
-												╳
-											</Button>
-										</div>
-									</>
-								)}
-							</div>
-						</div>
-					))
-				) : (
-					<p>No addresses available</p>
-				)}
-				<Form onSubmit={handleAddAddress}>
-					<Form.Group>
-						<Form.Control
-							type="text"
-							placeholder="Enter new address"
-							value={newAddress}
-							onChange={(e) => setNewAddress(e.target.value)}
-							style={{ marginBottom: "1rem", fontSize: "1.5rem" }}
-						/>
-					</Form.Group>
-					<Button type="submit">Add address</Button>
-				</Form>
+			</div>
+			<div>
+				{/* Button to return to home page */}
+				<Link to="/">
+					<Button
+						style={{
+							background: "#557C56",
+							color: "white",
+							marginTop: "1rem",
+						}}
+					>
+						Home
+					</Button>
+				</Link>
 			</div>
 		</div>
 	);
